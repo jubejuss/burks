dec3ae1484318e4eb7fc126b896ddf63aaa7f93b
Jubejuss <jubejuss@gmail.com>
2022-05-04 17:23:20 +0300

    first

---
+++ js.js
@@ -0,0 +1,19 @@
+/* const $ = document.querySelector.bind(document);
+let closeOpFunc = () => {
+    $('.nav-burger').classList.toggle('toggle');
+}
+$('.btn-nav').addEventListener("click", closeOpFunc); 
+$('.nav-burger').addEventListener("click", closeOpFunc); */
+
+/* const $ = document.querySelectorAll.bind(document);
+$('.btn-nav, .nav-burger').forEach(e => {
+    e.target.addEventListener("click", () => {
+        $('nav-burger')[0].classList.toggle('toggle');
+    });
+}); */
+
+document.querySelectorAll('.btn-nav, .nav-burger').forEach(e => {
+    e.target.addEventListener("click", () => {
+        $('nav-burger')[0].classList.toggle('toggle');
+    });
+});
